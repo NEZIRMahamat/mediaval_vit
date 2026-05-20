fe1f7333654f73d7665b75a74be2a5af6af21c9f
NEZIRMahamat <95191142+NEZIRMahamat@users.noreply.github.com>
2026-05-20 14:26:00 +0200

    fix: séparation model et exécution

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
-from ultralytics import YOLO 
-name_model = "magistermilitum/YOLO_manuscripts" # 
-from huggingface_hub import hf_hub_download 
+import os
 
-path = hf_hub_download(repo_id='magistermilitum/YOLO_manuscripts', 
-                       filename='best.pt' ) 
-print(path)
+from model import (
+    load_model_from_hf, predict_and_process, build_rectangles_on_image,
+    INPUT_DIR
+)
 
-# coords peut être [[[x1,y1],...]] ou [[x1,y1],...]
-# print("Modèle chargé")
+
+def main():
+    # parcourir images du dossier d'entrée : traitement par image
+    name_model = "magistermilitum/YOLO_manuscripts"
+    model = load_model_from_hf(name_model, 'best.pt')
+    for image_file in os.listdir(INPUT_DIR):
+        if image_file.lower().endswith(('.png', '.jpg', '.jpeg', '.bmp', '.tiff')):
+            image_path = os.path.join(INPUT_DIR, image_file)
+            print(f"Traitement de l'image : {image_path}")            
+            output_data = predict_and_process(model=model, image_path=image_path)
+            build_rectangles_on_image(output_data)
+
+if __name__ == "__main__":    
+    main()
--- model.py
+++ model.py
@@ -122,19 +122,6 @@ def build_rectangles_on_image(output_data):
     except Exception as e:
         print(f"Erreur lors de la construction des rectangles sur l'image {image_name} : {e}")
 
-def main():
-    # parcourir images du dossier d'entrée : traitement par image
-    name_model = "magistermilitum/YOLO_manuscripts"
-    model = load_model_from_hf(name_model, 'best.pt')
-    for image_file in os.listdir(INPUT_DIR):
-        if image_file.lower().endswith(('.png', '.jpg', '.jpeg', '.bmp', '.tiff')):
-            image_path = os.path.join(INPUT_DIR, image_file)
-            print(f"Traitement de l'image : {image_path}")            
-            output_data = predict_and_process(model=model, image_path=image_path)
-            build_rectangles_on_image(output_data)
-
-if __name__ == "__main__":    
-    main()
 
 
 
